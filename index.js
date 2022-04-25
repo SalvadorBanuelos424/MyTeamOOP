08265311c3b07b89865fad6f3adba8a4feddbd48
Sal <salvador.banuelos.424@gmail.com>
2022-04-25 13:35:50 -0700

    ready

---
+++ index.js
@@ -0,0 +1,4 @@
+// 4 Classes [Employee][Manager][Engineer][Intern
+// Properties&Methods [name][id][email][getName()][getId()][getEmail()][getRole()"Employee, Manager, Engineer, OR Intern"]
+// Add [officeNumber]to Manager.  [github][getGithub]to Engineer. [school][getSchool]to Intern
+// Include Validation
